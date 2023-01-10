507c1e6eafbc5ab36c09ab40ed91f41c9c609f3a
Godwill <gtdembo@gmail.com>
2023-01-10 14:08:08 -0800

    task

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,11 +8,9 @@ import json
 
 
 def to_json_string(my_obj):
-   """Returns th JSON representation of my_obj.
-
+    """Returns th JSON representation of my_obj.
     Args:
         - my_obj: string to represent
-
     Returns: JSON representation
     """
 
